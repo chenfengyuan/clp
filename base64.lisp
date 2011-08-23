9458b39680375cf4f8d669f5683879743c1c2639
Fengyuan Chen <cfy1990@gmail.com>
2011-08-23 16:51:13 +0800

    fixed undefined function bug

--- base64.lisp
+++ base64.lisp
@@ -1,11 +1,11 @@
 #+ccl
 (defun encode64(string)
   (cl-base64:string-to-base64-string
-   (decode-string-from-octets
-    (encode-string-to-octets string :external-format :utf-8))))
+   (ccl:decode-string-from-octets
+    (ccl:encode-string-to-octets string :external-format :utf-8))))
 #+ccl
 (defun decode64(string)
-  (decode-string-from-octets
-   (encode-string-to-octets
+  (ccl:decode-string-from-octets
+   (ccl:encode-string-to-octets
     (cl-base64:base64-string-to-string string))
    :external-format :utf-8))
